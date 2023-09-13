794fea53bc97eeba47f7a4113b4ea3fb301506a2
Natalya <m_d.r.lecter@protonmail.com>
2023-09-13 23:17:36 +0400

    template

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import image from './img/1.png';
+
+const Module = () => (
+  <>
+    <h1>Hello, React!</h1>
+    <img src={image}/>
+  </>
+);
+
+export default Module;
